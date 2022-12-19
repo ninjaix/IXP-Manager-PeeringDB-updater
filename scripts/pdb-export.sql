4d39b836cabace491c9d01a973af9ead1c96f138
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2022-12-18 19:44:26 -0700

    Update pdb-export.sql

--- scripts/pdb-export.sql
+++ scripts/pdb-export.sql
@@ -1,4 +1,4 @@
-SELECT `asn`,`name`,`info_never_via_route_servers`,`irr_as_set`,`info_prefixes4`,`info_prefixes6`,`status`,`updated` ,`website`,`policy_general`
+SELECT `asn`,`name`,`info_never_via_route_servers`,`irr_as_set`,`info_prefixes4`,`info_prefixes6`,`status`,`updated` ,`website`,`version`,`rir_status`,`policy_ratio`
 FROM `peeringdb`.`peeringdb_network`
 ORDER BY `asn`
 INTO OUTFILE '/var/lib/mysql-files/ixpm-pdb-network-1.csv'
