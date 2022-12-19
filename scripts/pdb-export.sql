b01089b5f6ef2dc387ff33ea8cc5dd845b9681ae
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2022-12-18 20:05:55 -0700

    Update pdb-export.sql

--- scripts/pdb-export.sql
+++ scripts/pdb-export.sql
@@ -1,7 +1,11 @@
-SELECT `asn`,`name`,`info_never_via_route_servers`,`irr_as_set`,`info_prefixes4`,`info_prefixes6`,`status`,`updated` ,`website`,`version`,`rir_status`,`policy_ratio`
+SELECT `asn`,`peeringdb_network`.`name`,`info_never_via_route_servers`,`irr_as_set`,`info_prefixes4`,`info_prefixes6`,
+`peeringdb_network`.`status`,`peeringdb_network`.`updated` ,`peeringdb_network`.`website`,
+`peeringdb_network`.`version`,`rir_status`,`policy_ratio`,`peeringdb_organization`.`name`
 FROM `peeringdb`.`peeringdb_network`
+JOIN `peeringdb`.`peeringdb_organization`
+ON `peeringdb_organization`.`id` = `peeringdb_network`.`org_id`
 ORDER BY `asn`
-INTO OUTFILE '/var/lib/mysql-files/ixpm-pdb-network-1.csv'
+INTO OUTFILE '/var/lib/mysql-files/ixpm-pdb-network.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
