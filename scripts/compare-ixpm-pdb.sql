f576301e563256c29f9abf3e9734710f5e754283
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2022-12-18 19:11:13 -0700

    Delete compare-ixpm-pdb.sql

--- scripts/compare-ixpm-pdb.sql
+++
@@ -1,4 +0,0 @@
-SELECT `cust`.`name`, `maxprefixes`,`peeringmacro`,`autsys`,`pdb_networks`.`as-set`,`pdb_networks`.`info_prefixes4`
-FROM `cust` 
-JOIN`ixpmanager`.`pdb_networks`
-ON `pdb_networks`.`asn` = `cust`.`autsys`;
