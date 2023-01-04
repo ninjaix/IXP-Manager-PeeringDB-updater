b0452547c9c46f379c578d742e6493b2fb3a1615
Paul Emmons <76638404+ninjaix@users.noreply.github.com>
2023-01-03 23:42:41 -0700

    Update pdb-export.sql

--- scripts/pdb-export.sql
+++ scripts/pdb-export.sql
@@ -1,3 +1,4 @@
+/*  the last 3 fields should really just be palce holders for the csv import- they are not actually used in IXP-Manager*/
 SELECT `asn`,`peeringdb_network`.`name`,`info_never_via_route_servers`,`irr_as_set`,`info_prefixes4`,`info_prefixes6`,
 `peeringdb_network`.`status`,`peeringdb_network`.`updated` ,`peeringdb_network`.`website`,
 `peeringdb_network`.`version`,`rir_status`,`policy_ratio`,`peeringdb_organization`.`name`
